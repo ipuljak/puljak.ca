16042176a5c4731e835667fc1fa4495d9e6849f4
Ivan Puljak <ivan@puljak.ca>
2018-12-28 21:31:54 -0500

    Update photo container styling

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-s3Path="s3://$1"
-
-npm run build
-
-aws s3 rm $s3Path --recursive
-aws s3 sync public $s3Path --acl public-read
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,5 @@ s3Path="s3://$1"
 
 npm run build
 
-aws s3 rm $s3Path --recursive
+aws s3 rm $s3Path bundle.js index.html
 aws s3 sync public $s3Path --acl public-read
