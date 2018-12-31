623d226675731d0a65dfdc02eccd11f216b8180e
Ivan Puljak <ivan@puljak.ca>
2018-12-31 00:18:05 -0500

    Updated website with resume section

--- app/pages/HomePage/Sections/Resume/Resume.js
+++ app/pages/HomePage/Sections/Resume/Resume.js
@@ -9,8 +9,11 @@ class Resume extends Component {
     render() {
         return (
             <section className="section" id={AppConstants.SECTIONS.RESUME.ID}>
-                <div>
-                    This is the Resume section.
+                <div className="section-title">Résumé</div>
+                <div className="section-resume-container">
+                    <object data="/files/resume.pdf" type="application/pdf" width="70%" height="800px">
+                        <p>It appears you don't have a PDF plugin for this browser. You can <a href="resume.pdf">click here to download the PDF file.</a></p>
+                    </object>
                 </div>
             </section>
         )
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,7 @@
 s3Path="s3://$1"
 
 npm run build
+mkdir -p public/files
+cp -a files/resume.pdf public/files/resume.pdf
 
-aws s3 rm $s3Path bundle.js index.html
 aws s3 sync public $s3Path --acl public-read
