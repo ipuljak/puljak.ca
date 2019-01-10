70d38257b12651f64387955a0aeaa16ae52399f2
Ivan Puljak <ivan@puljak.ca>
2019-01-09 22:49:32 -0500

    Updated publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-s3Path="s3://$1"
+S3_PATH="s3://$1"
 
 npm run build
 mkdir -p public/files
 cp -a files/resume.pdf public/files/resume.pdf
 
-aws s3 sync public $s3Path --acl public-read
+aws s3 sync public ${S3_PATH} --acl public-read
