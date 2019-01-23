328db9ca62f6a5a36fe760ce0e8e3169d9be8a6b
ipuljak <ivan@puljak.ca>
2019-01-22 23:09:37 -0500

    Updated bio section

--- app/pages/HomePage/Sections/Bio/Bio.js
+++ app/pages/HomePage/Sections/Bio/Bio.js
@@ -26,30 +26,30 @@ class Bio extends Component {
                             <p>Hello, my name is Ivan, and welcome to my personal website! This site more or less
                                 functions as my primary online presence as I don't use any social media. I am a
                                 professional software developer based out of Toronto, Ontario, and I am experienced in
-                                engineering full stack web applications and other software solutions. I mostly work with
+                                engineering full stack web applications and software solutions. I mostly work with
                                 JavaScript, though I am well versed in Python, Java, Go, and so much more.</p>
 
                             <p className="bio-title">Education & Work</p>
 
                             <p>I went to school at the University of Toronto and in 2014 I graduated with an Honours
                                 Bachelor of Science in Computer Science. To support myself, I worked as a truck driver,
-                                and later supervisor for a company called Arctic Glacier. I would later voluntarily
-                                leave to focus on expanding my abilities as a software developer.</p>
+                                and later distribution supervisor & dispatcher for a company called Arctic Glacier. I
+                                would later voluntarily leave to focus on expanding my abilities as a software developer.</p>
 
                             <p>In April of 2017, I started my career in technology after being hired as a software
                                 developer for a young ed-tech startup named <a href="https://getjoezoo.com" target="_blank">JoeZoo</a>.
                                 JoeZoo provides proof-of-learning and assessment tools for teachers and students and is
                                 currently used by 5 million users and 12 thousand schools across the world. I work on
-                                the entire stack, which includes JavaScript/React front-end, Grails/Groovy back-end, and
-                                dev-ops using many of the Amazon Web Services tools.</p>
+                                the entire stack, which includes a JavaScript/React front-end, Grails/Groovy back-end,
+                                and dev-ops using many of the Amazon Web Services tools.</p>
 
                             <p>A year later I was promoted to UI Lead, where I was given full responsibility for
                                 maintaining and improving the front-end code bases, as well as reviewing my team's work.
                                 During this time I was also given an opportunity to focus on a side project of my
                                 choice, and I decided to pick the study of natural language programming (NLP). Over the
-                                summer I built an Originality Checker written in Python and deployed on it's own Flask
-                                server as a microservice. In the future I look forward to working on topic modelling,
-                                sentiment analysis, and much more.</p>
+                                summer I built an Originality Checker written in Python and deployed it on it's own
+                                Flask server as a microservice. In the future I look forward to working on topic
+                                modelling, sentiment analysis, and much more.</p>
 
                             <p className="bio-title">Hobbies & Passions</p>
 
@@ -59,13 +59,13 @@ class Bio extends Component {
                                 guitar. I also enjoy reading, especially classic literature novels and books and ancient
                                 civilizations and world history. On the weekends, I also like to pass the time by
                                 helping others and being a part of my community through volunteering at Habitat for
-                                Humanity where I help out at the local ReStore. </p>
+                                Humanity where I help out at the local ReStore.</p>
 
                             <p>My passion for helping others stretches beyond what I've outlined on my website, with a
                                 dream to one day become a police officer in Canada. My desire to join a police force
                                 reaches back ever since I could remember. The greatest joy in my life comes from helping
                                 others and solving problems and I have always felt that a career as an involved member
-                                of my community is the best way to lead a fulfilling life.</p>
+                                of my community is the best way to lead a fulfilling and happy life.</p>
 
                             <p>If you'd like to learn more about me, or if you have any questions, please don't hesitate to
                                 contact me!</p>
