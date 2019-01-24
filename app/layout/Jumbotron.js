28e11e7bd0e63490d62b1b645867b625941201c6
ipuljak <ivan@puljak.ca>
2019-01-23 20:47:19 -0500

    Gallery page styling updates

--- app/layout/Jumbotron.js
+++ app/layout/Jumbotron.js
@@ -4,6 +4,7 @@ const Jumbotron = props => {
     return (
         <div className="jumbotron">
             <div className="jumbotron-title">{props.config.title}</div>
+            <hr className="jumbotron-hr" />
             <div className="jumbotron-description">{props.config.description}</div>
         </div>
     )
